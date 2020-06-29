0dc9b65135ada696632970ecdecc384f972c1dab
wndrlsla <wndrlsla0919@gmail.com>
2020-06-29 17:50:34 +0900

    0629

--- src/main/java/mybatis/MybatisDAOImpl.java
+++ src/main/java/mybatis/MybatisDAOImpl.java
@@ -2,8 +2,18 @@ package mybatis;
 
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Service;
+@Service
 public interface MybatisDAOImpl {
-
-	public ArrayList<MyBoardDTO> list();
+	
+	//public ArrayList<MyBoardDTO> list();
+	
+	/*
+	방명록리스트에서 사용할 메소드를 추상메소드로 정의함
+	아래 추상메소드를 통해 컨트롤러는 Mapper의 각 엘리먼트를
+	호출하게 된다.
+	 */
+	public int getTotalCount();
+	public ArrayList<MyBoardDTO> listPage(int s, int e);
 	
 }
---
+++ src/main/java/mybatis/MybatisMemberImpl.java
@@ -0,0 +1,6 @@
+package mybatis;
+
+public interface MybatisMemberImpl {
+
+	public MemberVO login(String id, String pass);
+}
